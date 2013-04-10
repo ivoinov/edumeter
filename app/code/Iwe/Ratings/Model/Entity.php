84f7075ad0b86a1813c88b19c8fa9af01254daad
Ilya Voinov <ilya.voinov@yahoo.com>
2013-04-11 00:36:31 +0300

    Add rating entity

---
+++ app/code/Iwe/Ratings/Model/Entity.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ivoinov
+ * Date: 4/11/13
+ * Time: 12:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Iwe_Ratings_Model_Entity extends Core_Model_Entity
+{
+
+}
---
+++ app/code/Iwe/Ratings/Resource/Entity.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ivoinov
+ * Date: 4/11/13
+ * Time: 12:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Iwe_Ratings_Resource_Entity extends Core_Resource_Entity
+{
+
+}
