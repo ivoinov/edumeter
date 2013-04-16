498ba78a54727474b1a03a8caf5b00bd44cfe6d9
Ilya Voinov <ilya.voinov@yahoo.com>
2013-04-16 20:40:02 +0300

    Add region entity

---
+++ app/code/Iwe/Region/Resource/Entity.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ivoinov
+ * Date: 4/16/13
+ * Time: 8:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Iwe_Region_Resource_Entity extends Core_Resource_Entity
+{
+
+}
