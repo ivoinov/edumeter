07546321001008305a7ff1a95b916b1c32e3f48f
Ilya Voinov <ilya.voinov@yahoo.com>
2013-04-21 23:34:00 +0300

    Add permanently redirect from home page

--- app/code/Iwe/Dashboard/Controller/Index.php
+++ app/code/Iwe/Dashboard/Controller/Index.php
@@ -3,6 +3,7 @@
 	class Iwe_Dashboard_Controller_Index extends Core_Controller_Index {
 		
 		public function indexAction() {
+            $this->redirect('school',301);
             $this->getLayout()->addTag("dashboard");
 			$this->render();			
 		}
